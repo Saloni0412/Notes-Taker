bb0c0f87e95e2fbd72b6daec7c0717581bd01b94
Saloni0412 <pshashikantb@gmail.com>
2023-05-23 20:18:43 -0400

    this commit adds uuid key in input.js and clears few errors

--- routes/input.js
+++ routes/input.js
@@ -1,6 +1,9 @@
 const input = require('express').Router();
 const fs = require("fs");
 const dbNotes = require("../db/db.json")
+const { v4: uuidv4 } = require('uuid');
+const newUUID = uuidv4();
+const path = "..\db\db.json";
 
 
 // GET Route for retrieving all the notes
@@ -12,29 +15,29 @@ input.get("/notes", (req, res) => {
 input.post('/notes', (req, res) => {
   const { title, text } = req.body;
 
-  if (title && text) {
+  if (title && text ) {
     const newInput = {
       title,
       text,
-      // feedback_id: uuidv4(),
+      uuid: newUUID,
     };
 
    dbNotes.push(newInput);
 
    let notesString = JSON.stringify(dbNotes, null, 3);
 
-   fs.writeFile(`../db/db.json`, notesString, (err) =>
-     err ? console.error(err) : console.log(`New note has been added!`)
+   fs.writeFile(path, notesString, (err) =>
+     err ? console.error(err) : console.log(`New note is saved`)
    );
 
     const response = {
       status: 'success',
       body: newInput,
     };
-
+console.log(newInput)
     res.json(response);
   } else {
-    res.json('Error in posting notes');
+    res.json('Error in saving note');
   }
 });
 
